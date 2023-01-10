b65980e509b10d4717caeccf40280947e742f007
Shane Hingtgen <shanehingtgen@gmail.com>
2023-01-09 21:45:54 -0600

    assignment 2.2

---
+++ week-2/sports-app/index.js
@@ -0,0 +1,15 @@
+/*
+==============================================================================
+; Title: index.js
+; Author: Professor Krasso
+; Modified by: Shane Hingtgen
+; Bellevue University
+; Date: 01/09/23
+; Description: This is the index.js file for the week-2 assignment 2 dependencies, installing two dependencies
+; Work Cited: 
+    Web 330 HTML, CSS, and JavaScript Requirements
+    Web340_Assign2
+    Web340_Week2 PowerPoint
+=================================================================================================================
+*/
+
