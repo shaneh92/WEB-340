23b6ed7aa142b837b3b7b52c00524ec5992da427
Shane Hingtgen <shanehingtgen@gmail.com>
2023-02-10 20:52:54 -0600

    finished this weeks project

--- week-6/web-340-master/week-6/fms/index.js
+++ week-6/web-340-master/week-6/fms/index.js
@@ -1,78 +1,81 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
 
-const Fruit = require('./models/fruit');
+const Fruit = require("./models/fruit");
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true })); // added during week 6 assignment
 app.use(express.json()); // added during week 6 assignment
 
 const PORT = process.env.PORT || 3000;
 
-const conn = 'mongodb+srv://web340_user:admin@cluster0.lujih.mongodb.net/web340DB?retryWrites=true&w=majority'; // added in week 6
+const conn =
+  "mongodb+srv://web340_admin:weider123@bellevueuniversity.ut5xprd.mongodb.net/?retryWrites=true&w=majority"; // added in week 6
 
 // added in week 6
-mongoose.connect(conn).then(() => {
-    console.log('Connection to the database was successful');
-}).catch(err => {
-    console.log('MongoDB Error: ' + err.message);
-})
+mongoose
+  .connect(conn)
+  .then(() => {
+    console.log("Connection to the database was successful");
+  })
+  .catch((err) => {
+    console.log("MongoDB Error: " + err.message);
+  });
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'FMS: Landing'
-    })
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "FMS: Landing",
+  });
 });
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'FMS: About',
-        pageTitle: 'Our Story!'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "FMS: About",
+    pageTitle: "Our Story!",
+  });
+});
 
-app.get('/contact', (req, res) => {
-    res.render('contact', {
-        title: 'FMS: Contact',
-        pageTitle: 'Our Locations!'
-    })
-})
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    title: "FMS: Contact",
+    pageTitle: "Our Locations!",
+  });
+});
 
-app.get('/fruit-create', (req, res) => {
-    res.render('fruit-create', {
-        title: 'FMS: Fruit Form',
-        pageTitle: 'Create Fruit'
-    })
-})
+app.get("/fruit-create", (req, res) => {
+  res.render("fruit-create", {
+    title: "FMS: Fruit Form",
+    pageTitle: "Create Fruit",
+  });
+});
 
-app.post('/fruits', (req, res, next) => {
-    console.log(req.body);
-    console.log(req.body.fruitName);
-    const newFruit = new Fruit({
-        name: req.body.fruitName
-    })
+app.post("/fruits", (req, res, next) => {
+  console.log(req.body);
+  console.log(req.body.fruitName);
+  const newFruit = new Fruit({
+    name: req.body.fruitName,
+  });
 
-    console.log(newFruit);
+  console.log(newFruit);
 
-    Fruit.create(newFruit, function(err, fruit) {
-        if (err) {
-            console.log(err);
-            next(err);
-        } else {
-            res.render('index', {
-                title: 'FMS: Landing'
-            })
-        }
-    })
-})
+  Fruit.create(newFruit, function (err, fruit) {
+    if (err) {
+      console.log(err);
+      next(err);
+    } else {
+      res.render("index", {
+        title: "FMS: Landing",
+      });
+    }
+  });
+});
 
 app.listen(PORT, () => {
-    console.log('Application started and listening on PORT ' + PORT);
+  console.log("Application started and listening on PORT " + PORT);
 });
-
